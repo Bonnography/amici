3145b315b0d5bc9ea92f968a04d74874150dd331
bbom <benjamin.bomberg@stroeer-dialog.de>
2024-03-28 22:11:15 +0100

    lazy load

--- Build/js/cb_template.js
+++ Build/js/cb_template.js
@@ -311,6 +311,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
         });
     }
+    // Function to check if an element is in the viewport
+    function isElementVisible(el) {
+        let rect     = el.getBoundingClientRect(),
+            vWidth   = window.innerWidth || document.documentElement.clientWidth,
+            vHeight  = window.innerHeight || document.documentElement.clientHeight,
+            efp      = function (x, y) {
+            return document.elementFromPoint(x, y);
+        };
+
+        // Return false if it's not in the viewport
+        if (rect.right < 0 || rect.bottom < 0
+            || rect.left > vWidth || rect.top > vHeight)
+            return false;
+
+        // Return true if any of its four corners are visible
+        return (
+            el.contains(efp(rect.left,  rect.top))
+            ||  el.contains(efp(rect.right, rect.top))
+            ||  el.contains(efp(rect.right, rect.bottom))
+            ||  el.contains(efp(rect.left,  rect.bottom))
+        );
+    }
+// Function to lazily load content
+    function lazyLoadContent() {
+        const lazyContentElements = document.querySelectorAll(".lazy-content");
+        lazyContentElements.forEach((element) => {
+            console.log(element);
+            console.log(isElementVisible(element));
+            if (isElementVisible(element)) {
+
+                // Add your logic to load the content for the element here
+                element.classList.add("loaded");
+            }
+        });
+    }
+// Attach the lazyLoadContent function to the scroll event
+    window.addEventListener("scroll", lazyLoadContent);
+// Call the function initially to load the visible content on page load
+    lazyLoadContent();
 });
 
 
